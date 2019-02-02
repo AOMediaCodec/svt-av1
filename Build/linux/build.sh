a0c4df44491a813c02eabbc6f615355c2bfc8d76
tux3 <barrdetwix@gmail.com>
2019-02-02 23:32:38 +0100

    build.sh: Clean whitespace

--- Build/linux/build.sh
+++ Build/linux/build.sh
@@ -1,45 +1,45 @@
-# 
+#
 # Copyright(c) 2019 Intel Corporation
 # SPDX - License - Identifier: BSD - 2 - Clause - Patent
-# 
+#
 
 #!/bin/bash
 
 function clean {
-	rm -R -f debug
-	rm -R -f release
+    rm -R -f debug
+    rm -R -f release
     rm -R -f ../../Bin/Debug
     rm -R -f ../../Bin/Release
 }
 
 function debug {
-	mkdir -p debug
-	mkdir -p ../../Bin/Debug
-	cd debug
-	PATH=$PATH:/usr/local/bin/
-	cmake ../../.. 					\
-		-DCMAKE_BUILD_TYPE=Debug			\
-		-DCMAKE_C_COMPILER=$CMAKE_COMPILER		\
-		-DCMAKE_ASM_NASM_COMPILER=$CMAKE_ASSEMBLER	\
-		
-	# Compile the Library	
-	make SvtAv1EncApp
-	cd ..
+    mkdir -p debug
+    mkdir -p ../../Bin/Debug
+    cd debug
+    PATH=$PATH:/usr/local/bin/
+    cmake ../../..                                  \
+        -DCMAKE_BUILD_TYPE=Debug                    \
+        -DCMAKE_C_COMPILER=$CMAKE_COMPILER          \
+        -DCMAKE_ASM_NASM_COMPILER=$CMAKE_ASSEMBLER  \
+
+    # Compile the Library
+    make SvtAv1EncApp
+    cd ..
 }
 
 function release {
-	mkdir -p release
-	mkdir -p ../../Bin/Release
-	cd release
-	PATH=$PATH:/usr/local/bin/
-	cmake ../../.. 					\
-		-DCMAKE_BUILD_TYPE=Release			\
-		-DCMAKE_C_COMPILER=$CMAKE_COMPILER		\
-		-DCMAKE_ASM_NASM_COMPILER=$CMAKE_ASSEMBLER	\
-	
-	# Compile the Library	
-	make SvtAv1EncApp
-        cd  ..	
+    mkdir -p release
+    mkdir -p ../../Bin/Release
+    cd release
+    PATH=$PATH:/usr/local/bin/
+    cmake ../../..                                  \
+        -DCMAKE_BUILD_TYPE=Release                  \
+        -DCMAKE_C_COMPILER=$CMAKE_COMPILER          \
+        -DCMAKE_ASM_NASM_COMPILER=$CMAKE_ASSEMBLER  \
+
+    # Compile the Library
+    make SvtAv1EncApp
+    cd ..
 }
 
 # Defines
@@ -48,35 +48,35 @@ GCC_COMPILER=gcc
 ICC_COMPILER=/opt/intel/composerxe/bin/icc
 
 if [ ! -e $ICC_COMPILER ]; then
-	CMAKE_COMPILER=$GCC_COMPILER
+    CMAKE_COMPILER=$GCC_COMPILER
 elif [ "$1" == "gcc" ]; then
-	CMAKE_COMPILER=$GCC_COMPILER
+    CMAKE_COMPILER=$GCC_COMPILER
 elif [ "$2" == "gcc" ]; then
-	CMAKE_COMPILER=$GCC_COMPILER
+    CMAKE_COMPILER=$GCC_COMPILER
 else
-	CMAKE_COMPILER=$ICC_COMPILER
+    CMAKE_COMPILER=$ICC_COMPILER
 fi
 
 if [ $# -eq 0 ]; then
-	debug
-	release
+    debug
+    release
 elif [ "$1" = "clean" ]; then
-	clean
+    clean
 elif [ "$1" = "debug" ]; then
-	debug
+    debug
 elif [ "$1" = "release" ]; then
-	release
+    release
 elif [ "$1" = "cpp" ]; then
-	debug
-	release
+    debug
+    release
 elif [ "$1" = "all" ]; then
-	debug
-	release
+    debug
+    release
 elif [ "$1" = "gcc" ]; then
-	debug
-	release
+    debug
+    release
 else
-	echo "build.sh <clean|all|debug|release|help>"
+    echo "build.sh <clean|all|debug|release|help>"
 fi
 
 exit
