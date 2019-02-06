fe842855381b0547f506e59d595187d687693010
Timo Witte <timo.witte@gmail.com>
2019-02-07 00:33:05 +0100

    change datatypes to avoid a lot of casting

--- Source/App/EbAppInputy4m.c
+++ Source/App/EbAppInputy4m.c
@@ -12,9 +12,8 @@
 int32_t readY4mHeader(EbConfig_t *cfg){
 
     FILE *ptr_in;
-    unsigned char buffer[YFM_HEADER_MAX];
-    unsigned char *tokstart, *tokend;
-    char *fresult;
+    char buffer[YFM_HEADER_MAX];
+    char *fresult, *tokstart, *tokend;
     uint32_t bitdepth = 8, width = 0, height = 0, fr_n = 0,
         fr_d = 0, aspect_n, aspect_d;
     char chroma[4] = "420", scan_type = 'p';
@@ -23,13 +22,13 @@ int32_t readY4mHeader(EbConfig_t *cfg){
     ptr_in = cfg->inputFile;
 
     /* get first line after YUV4MPEG2 */
-    fresult = fgets((char*)buffer, sizeof(buffer), ptr_in);
+    fresult = fgets(buffer, sizeof(buffer), ptr_in);
     assert(fresult != NULL);
 
     /* print header */
     if(PRINT_HEADER) {
         printf("y4m header:");
-        fputs((const char *) buffer, stdout);
+        fputs(buffer, stdout);
     }
 
     /* read header parameters */
@@ -38,13 +37,13 @@ int32_t readY4mHeader(EbConfig_t *cfg){
             continue;
         switch (*tokstart++) {
         case 'W': /* width, required. */
-            width = strtol((const char*)tokstart, (char**)&tokend, 10);
+            width = strtol(tokstart, &tokend, 10);
             if(PRINT_HEADER)
                 printf("width = %d\n", width);
             tokstart = tokend;
             break;
         case 'H': /* height, required. */
-            height = strtol((const char*)tokstart, (char**)&tokend, 10);
+            height = strtol(tokstart, &tokend, 10);
             if(PRINT_HEADER)
                 printf("height = %d\n", height);
             tokstart = tokend;
@@ -71,73 +70,73 @@ int32_t readY4mHeader(EbConfig_t *cfg){
                 printf("scan_type = %c\n", scan_type);
             break;
         case 'C': /* color space, not required: default "420" */
-            if (strncmp("420p16", (const char*)tokstart, 6) == 0) {
+            if (strncmp("420p16", tokstart, 6) == 0) {
                 strcpy(chroma, "420");
                 bitdepth = 16;
-            } else if (strncmp("422p16", (const char*)tokstart, 6) == 0) {
+            } else if (strncmp("422p16", tokstart, 6) == 0) {
                  strcpy(chroma, "422");
                  bitdepth = 16;
-            } else if (strncmp("444p16", (const char*)tokstart, 6) == 0) {
+            } else if (strncmp("444p16", tokstart, 6) == 0) {
                 strcpy(chroma, "444");
                 bitdepth = 16;
-            } else if (strncmp("420p14", (const char*)tokstart, 6) == 0) {
+            } else if (strncmp("420p14", tokstart, 6) == 0) {
                 strcpy(chroma, "420");
                 bitdepth = 14;
-            } else if (strncmp("422p14", (const char*)tokstart, 6) == 0) {
+            } else if (strncmp("422p14", tokstart, 6) == 0) {
                 strcpy(chroma, "422");
                 bitdepth = 14;
-            } else if (strncmp("444p14", (const char*)tokstart, 6) == 0) {
+            } else if (strncmp("444p14", tokstart, 6) == 0) {
                 strcpy(chroma, "444");
                 bitdepth = 14;
-            } else if (strncmp("420p12", (const char*)tokstart, 6) == 0) {
+            } else if (strncmp("420p12", tokstart, 6) == 0) {
                 strcpy(chroma, "420");
                 bitdepth = 12;
-            } else if (strncmp("422p12", (const char*)tokstart, 6) == 0) {
+            } else if (strncmp("422p12", tokstart, 6) == 0) {
                 strcpy(chroma, "422");
                 bitdepth = 12;
-            } else if (strncmp("444p12", (const char*)tokstart, 6) == 0) {
+            } else if (strncmp("444p12", tokstart, 6) == 0) {
                 strcpy(chroma, "444");
                 bitdepth = 12;
-            } else if (strncmp("420p10", (const char*)tokstart, 6) == 0) {
+            } else if (strncmp("420p10", tokstart, 6) == 0) {
                 strcpy(chroma, "420");
                 bitdepth = 10;
-            } else if (strncmp("422p10", (const char*)tokstart, 6) == 0) {
+            } else if (strncmp("422p10", tokstart, 6) == 0) {
                 strcpy(chroma, "422");
                 bitdepth = 10;
-            } else if (strncmp("444p10", (const char*)tokstart, 6) == 0) {
+            } else if (strncmp("444p10", tokstart, 6) == 0) {
                 strcpy(chroma, "444");
                 bitdepth = 10;
-            } else if (strncmp("420p9", (const char*)tokstart, 5) == 0) {
+            } else if (strncmp("420p9", tokstart, 5) == 0) {
                 strcpy(chroma, "420");
                 bitdepth = 9;
-            } else if (strncmp("422p9", (const char*)tokstart, 5) == 0) {
+            } else if (strncmp("422p9", tokstart, 5) == 0) {
                 strcpy(chroma, "422");
                 bitdepth = 9;
-            } else if (strncmp("444p9", (const char*)tokstart, 5) == 0) {
+            } else if (strncmp("444p9", tokstart, 5) == 0) {
                 strcpy(chroma, "444");
                 bitdepth = 9;
-            } else if (strncmp("420", (const char*)tokstart, 3) == 0) {
+            } else if (strncmp("420", tokstart, 3) == 0) {
                 strcpy(chroma, "420");
                 bitdepth = 8;
-            } else if (strncmp("411", (const char*)tokstart, 3) == 0) {
+            } else if (strncmp("411", tokstart, 3) == 0) {
                 strcpy(chroma, "411");
                 bitdepth = 8;
-            } else if (strncmp("422", (const char*)tokstart, 3) == 0) {
+            } else if (strncmp("422", tokstart, 3) == 0) {
                 strcpy(chroma, "422");
                 bitdepth = 8;
-            } else if (strncmp("mono16", (const char*)tokstart, 6) == 0) {
+            } else if (strncmp("mono16", tokstart, 6) == 0) {
                 strcpy(chroma, "400");
                 bitdepth = 16;
-            } else if (strncmp("mono12", (const char*)tokstart, 6) == 0) {
+            } else if (strncmp("mono12", tokstart, 6) == 0) {
                 strcpy(chroma, "400");
                 bitdepth = 12;
-            } else if (strncmp("mono10", (const char*)tokstart, 6) == 0) {
+            } else if (strncmp("mono10", tokstart, 6) == 0) {
                 strcpy(chroma, "400");
                 bitdepth = 10;
-            } else if (strncmp("mono9", (const char*)tokstart, 5) == 0) {
+            } else if (strncmp("mono9", tokstart, 5) == 0) {
                 strcpy(chroma, "400");
                 bitdepth = 9;
-            } else if (strncmp("mono", (const char*)tokstart, 4) == 0) {
+            } else if (strncmp("mono", tokstart, 4) == 0) {
                 strcpy(chroma, "400");
                 bitdepth = 8;
             } else {
@@ -150,7 +149,7 @@ int32_t readY4mHeader(EbConfig_t *cfg){
                 printf("chroma = %s, bitdepth = %d\n", chroma, bitdepth);
             break;
         case 'F': /* frame rate, required */
-            sscanf((const char*)tokstart, "%d:%d", &fr_n, &fr_d); // 0:0 if unknown
+            sscanf(tokstart, "%d:%d", &fr_n, &fr_d); // 0:0 if unknown
             if(PRINT_HEADER) {
                 printf("framerate_n = %d\n", fr_n);
                 printf("framerate_d = %d\n", fr_d);
@@ -160,7 +159,7 @@ int32_t readY4mHeader(EbConfig_t *cfg){
             }
             break;
         case 'A': /* aspect ratio, not required */
-            sscanf((const char*)tokstart, "%d:%d", &aspect_n, &aspect_d); // 0:0 if unknown
+            sscanf(tokstart, "%d:%d", &aspect_n, &aspect_d); // 0:0 if unknown
             if(PRINT_HEADER) {
                 printf("aspect_n = %d\n", aspect_n);
                 printf("aspect_d = %d\n", aspect_d);
@@ -225,7 +224,7 @@ int32_t readY4mFrameDelimiter(EbConfig_t *cfg){
 /* check if the input file is in YUV4MPEG2 (y4m) format */
 EbBool checkIfY4m(EbConfig_t *cfg){
 
-    unsigned char buffer[YUV4MPEG2_IND_SIZE+1];
+    char buffer[YUV4MPEG2_IND_SIZE+1];
     size_t headerReadLength;
 
     /* Parse the header for the "YUV4MPEG2" string */
@@ -233,7 +232,7 @@ EbBool checkIfY4m(EbConfig_t *cfg){
     assert(headerReadLength == 1);
     buffer[YUV4MPEG2_IND_SIZE] = 0;
 
-    if (strcmp((const char*)buffer, "YUV4MPEG2") == 0) {
+    if (strcmp(buffer, "YUV4MPEG2") == 0) {
         return EB_TRUE; /* YUV4MPEG2 file */
     }else{
         if(cfg->inputFile != stdin) {
