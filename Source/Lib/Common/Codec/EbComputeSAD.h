7f278abbfd2758a0d75e684bed02a3d5ce0913e0
Jack Zhou <jack.zhou@intel.com>
2019-08-06 12:37:32 -0700

    cleanup unused posthoc func table types

--- Source/Lib/Common/Codec/EbComputeSAD.h
+++ Source/Lib/Common/Codec/EbComputeSAD.h
@@ -18,72 +18,8 @@
 extern "C" {
 #endif
 
-    uint32_t compute4x_m_sad_avx2_intrin(const uint8_t  *src, uint32_t  src_stride, const uint8_t  *ref, uint32_t  ref_stride, uint32_t  height, uint32_t  width);
-
-    /***************************************
-    * Function Ptr Types
-    ***************************************/
-
-    static void nxm_sad_kernel_void_func() {}
-
-    typedef void(*EbSadLoopKernelNxMType)(
-        uint8_t  *src,                            // input parameter, source samples Ptr
-        uint32_t  src_stride,                      // input parameter, source stride
-        uint8_t  *ref,                            // input parameter, reference samples Ptr
-        uint32_t  ref_stride,                      // input parameter, reference stride
-        uint32_t  height,                         // input parameter, block height (M)
-        uint32_t  width,                          // input parameter, block width (N)
-        uint64_t *best_sad,
-        int16_t *x_search_center,
-        int16_t *y_search_center,
-        uint32_t  src_stride_raw,                   // input parameter, source stride (no line skipping)
-        int16_t search_area_width,
-        int16_t search_area_height);
-
-
-    typedef uint32_t(*EbCompute8x4SadType)(
-        uint8_t  *src,                            // input parameter, source samples Ptr
-        uint32_t  src_stride,                      // input parameter, source stride
-        uint8_t  *ref,                            // input parameter, reference samples Ptr
-        uint32_t  ref_stride);                     // input parameter, reference stride
-
-    typedef uint32_t(*EB_COMPUTE8X8SAD_TYPE)(
-        uint8_t  *src,                            // input parameter, source samples Ptr
-        uint32_t  src_stride,                      // input parameter, source stride
-        uint8_t  *ref,                            // input parameter, reference samples Ptr
-        uint32_t  ref_stride);                     // input parameter, reference stride
-    typedef void(*EbGetEightSad8x8)(
-        uint8_t   *src,
-        uint32_t   src_stride,
-        uint8_t   *ref,
-        uint32_t   ref_stride,
-        uint32_t  *p_best_sad8x8,
-        uint32_t  *p_best_mv8x8,
-        uint32_t  *p_best_sad16x16,
-        uint32_t  *p_best_mv16x16,
-        uint32_t   mv,
-        uint16_t  *p_sad16x16,
-        EbBool     sub_sad);
-
-    typedef void(*EbGetEightSad32x32)(
-        uint16_t  *p_sad16x16,
-        uint32_t  *p_best_sad32x32,
-        uint32_t  *p_best_sad64x64,
-        uint32_t  *p_best_mv32x32,
-        uint32_t  *p_best_mv64x64,
-        uint32_t   mv);
-
-    typedef uint32_t(*CombinedAveragingSsd)(
-        uint8_t   *src,
-        ptrdiff_t  src_stride,
-        uint8_t   *ref1,
-        ptrdiff_t  ref1_stride,
-        uint8_t   *ref2,
-        ptrdiff_t  ref2_stride,
-        uint32_t   height,
-        uint32_t   width
-        );
-
+uint32_t compute4x_m_sad_avx2_intrin(const uint8_t  *src, uint32_t  src_stride, const uint8_t  *ref, uint32_t  ref_stride, uint32_t  height, uint32_t  width);
+    
 uint32_t sad_16b_kernel(
     uint16_t  *src,                           // input parameter, source samples Ptr
     uint32_t  src_stride,                     // input parameter, source stride
