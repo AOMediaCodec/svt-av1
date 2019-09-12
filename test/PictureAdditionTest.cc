332537fb74c4ca09024ccea3487d5e2755181764
ivy <ivy.zhang@cidana.com>
2019-09-12 13:10:47 +0800

    Compare with C implementation of Picture Addition
    1. compare LbdPictureAdditionFunc and HbdPictureAdditionFunc with
       c implementation
    2. fix a bug in run_16bit_test.

--- test/PictureAdditionTest.cc
+++ test/PictureAdditionTest.cc
@@ -111,7 +111,7 @@ class PictureAdditionTest : public ::testing::Test,
         residual_int32_ = nullptr;
         pred_ = nullptr;
         pred16bit_ = nullptr;
-        recon1_ = recon2_ = nullptr;
+        recon_1_ = recon_2_ = recon_c_ = nullptr;
         recon1_16bit_ = recon2_16bit_ = nullptr;
         recon_stride_ = pred_stride_ = residual_stride_ = MAX_PU_SIZE;
         test_size_ = MAX_PU_SIZE * MAX_PU_SIZE;
@@ -123,16 +123,19 @@ class PictureAdditionTest : public ::testing::Test,
         residual_int32_ = reinterpret_cast<int32_t *>(
             eb_aom_memalign(32, sizeof(int32_t) * test_size_));
         pred_ = reinterpret_cast<uint8_t *>(eb_aom_memalign(32, test_size_));
-        recon1_ = reinterpret_cast<uint8_t *>(eb_aom_memalign(32, test_size_));
-        recon2_ = reinterpret_cast<uint8_t *>(eb_aom_memalign(32, test_size_));
+        recon_1_ = reinterpret_cast<uint8_t *>(eb_aom_memalign(32, test_size_));
+        recon_2_ = reinterpret_cast<uint8_t *>(eb_aom_memalign(32, test_size_));
+        recon_c_ = reinterpret_cast<uint8_t *>(eb_aom_memalign(32, test_size_));
         pred16bit_ = reinterpret_cast<uint16_t *>(
             eb_aom_memalign(32, sizeof(uint16_t) * test_size_));
         recon1_16bit_ = reinterpret_cast<uint16_t *>(
             eb_aom_memalign(32, sizeof(uint16_t) * test_size_));
         recon2_16bit_ = reinterpret_cast<uint16_t *>(
             eb_aom_memalign(32, sizeof(uint16_t) * test_size_));
-        memset(recon1_, 0, test_size_ * sizeof(recon1_[0]));
-        memset(recon2_, 0, test_size_ * sizeof(recon2_[0]));
+        memset(residual_int32_, 0, test_size_ * sizeof(residual_int32_[0]));
+        memset(recon_1_, 0, test_size_ * sizeof(recon_1_[0]));
+        memset(recon_2_, 0, test_size_ * sizeof(recon_2_[0]));
+        memset(recon_c_, 0, test_size_ * sizeof(recon_c_[0]));
         memset(recon1_16bit_, 0, test_size_ * sizeof(recon1_16bit_[0]));
         memset(recon2_16bit_, 0, test_size_ * sizeof(recon2_16bit_[0]));
     }
@@ -150,29 +153,31 @@ class PictureAdditionTest : public ::testing::Test,
             eb_aom_free(recon2_16bit_);
         if (pred16bit_)
             eb_aom_free(pred16bit_);
-        if (recon1_)
-            eb_aom_free(recon1_);
-        if (recon2_)
-            eb_aom_free(recon2_);
+        if (recon_1_)
+            eb_aom_free(recon_1_);
+        if (recon_2_)
+            eb_aom_free(recon_2_);
+        if (recon_c_)
+            eb_aom_free(recon_c_);
     }
 
   protected:
     void prepare_data() {
-        const uint8_t mask_uint8 = (1 << 8) - 1;
-        const int16_t mask_int16 = (1 << 15) - 1;
-        SVTRandom rnd_uint8(0, mask_uint8);
-        SVTRandom rnd_int16(0, mask_int16);
+        const uint8_t mask_pred = (1 << 8) - 1;
+        const int16_t mask_res = (1 << 15) - 1;
+        SVTRandom rnd_uint8(0, mask_pred);
+        SVTRandom rnd_int16(0, mask_res);
         switch (test_pattern_) {
         case PRED_MIN: {
             for (uint32_t i = 0; i < test_size_; i++) {
                 pred_[i] = 0;
-                residual_[i] = residual_int32_[i] = mask_int16;
+                residual_[i] = residual_int32_[i] = mask_res;
             }
             break;
         }
         case PRED_MAX: {
             for (uint32_t i = 0; i < test_size_; i++) {
-                pred_[i] = mask_uint8;
+                pred_[i] = mask_pred;
                 residual_[i] = residual_int32_[i] = 0;
             }
             break;
@@ -186,8 +191,8 @@ class PictureAdditionTest : public ::testing::Test,
         }
         case ALL_MAX: {
             for (uint32_t i = 0; i < test_size_; i++) {
-                pred_[i] = mask_uint8;
-                residual_[i] = residual_int32_[i] = mask_int16;
+                pred_[i] = mask_pred;
+                residual_[i] = residual_int32_[i] = mask_res;
             }
             break;
         }
@@ -203,21 +208,22 @@ class PictureAdditionTest : public ::testing::Test,
     }
 
     void prepare_16bit_data() {
-        const uint16_t mask_uint16 = (1 << 16) - 1;
-        const int16_t mask_int16 = (1 << 15) - 1;
-        SVTRandom rnd_uint16(0, mask_uint16);
-        SVTRandom rnd_int16(0, mask_int16);
+        // Max bitdepth is 12, set the mask accordingly.
+        const uint16_t mask_pred = (1 << 12) - 1;
+        const int16_t mask_res = (1 << 15) - 1;
+        SVTRandom rnd_uint16(0, mask_pred);
+        SVTRandom rnd_int16(0, mask_res);
         switch (test_pattern_) {
         case PRED_MIN: {
             for (uint32_t i = 0; i < test_size_; i++) {
                 pred16bit_[i] = 0;
-                residual_[i] = residual_int32_[i] = mask_int16;
+                residual_[i] = residual_int32_[i] = mask_res;
             }
             break;
         }
         case PRED_MAX: {
             for (uint32_t i = 0; i < test_size_; i++) {
-                pred16bit_[i] = mask_uint16;
+                pred16bit_[i] = mask_pred;
                 residual_[i] = 0;
                 residual_int32_[i] = 0;
             }
@@ -233,8 +239,8 @@ class PictureAdditionTest : public ::testing::Test,
         }
         case ALL_MAX: {
             for (uint32_t i = 0; i < test_size_; i++) {
-                pred16bit_[i] = mask_uint16;
-                residual_[i] = residual_int32_[i] = mask_int16;
+                pred16bit_[i] = mask_pred;
+                residual_[i] = residual_int32_[i] = mask_res;
             }
             break;
         }
@@ -249,12 +255,27 @@ class PictureAdditionTest : public ::testing::Test,
         }
     }
 
-    void check_output(uint32_t width, uint32_t height) {
+    void check_output(uint32_t width, uint32_t height, uint8_t *out_1,
+                      uint8_t *out_2) {
         int fail_count = 0;
         for (uint32_t j = 0; j < height; j++) {
             for (uint32_t k = 0; k < width; k++) {
-                if (recon1_[k + j * recon_stride_] !=
-                    recon2_[k + j * recon_stride_])
+                if (out_1[k + j * recon_stride_] !=
+                    out_2[k + j * recon_stride_])
+                    fail_count++;
+            }
+        }
+        EXPECT_EQ(0, fail_count)
+            << "compare picture addition result error"
+            << "in test area for " << fail_count << "times";
+    }
+
+    void check_16bit_output(uint32_t width, uint32_t height) {
+        int fail_count = 0;
+        for (uint32_t j = 0; j < height; j++) {
+            for (uint32_t k = 0; k < width; k++) {
+                if (recon1_16bit_[k + j * recon_stride_] !=
+                    recon2_16bit_[k + j * recon_stride_])
                     fail_count++;
             }
         }
@@ -269,7 +290,7 @@ class PictureAdditionTest : public ::testing::Test,
                        pred_stride_,
                        residual_,
                        residual_stride_,
-                       recon1_,
+                       recon_1_,
                        recon_stride_,
                        area_width_,
                        area_height_);
@@ -277,13 +298,22 @@ class PictureAdditionTest : public ::testing::Test,
                        pred_stride_,
                        residual_int32_,
                        residual_stride_,
-                       recon2_,
+                       recon_2_,
                        recon_stride_,
                        area_width_,
                        area_height_,
-                       2);
-
-        check_output(area_width_, area_height_);
+                       8);
+        picture_addition_kernel(pred_,
+                                pred_stride_,
+                                residual_int32_,
+                                residual_stride_,
+                                recon_c_,
+                                recon_stride_,
+                                area_width_,
+                                area_height_,
+                                8);
+        check_output(area_width_, area_height_, recon_1_, recon_c_);
+        check_output(area_width_, area_height_, recon_2_, recon_c_);
         EXPECT_FALSE(HasFailure());
     }
 
@@ -305,16 +335,15 @@ class PictureAdditionTest : public ::testing::Test,
                                       recon_stride_,
                                       area_width_,
                                       area_height_,
-                                      2);
-
-        check_output(area_width_, area_height_);
+                                      10);
+        check_16bit_output(area_width_, area_height_);
         EXPECT_FALSE(HasFailure());
     }
 
     TestPattern test_pattern_;
     uint32_t area_width_, area_height_;
     uint32_t recon_stride_, pred_stride_, residual_stride_;
-    uint8_t *pred_, *recon1_, *recon2_;
+    uint8_t *pred_, *recon_1_, *recon_2_, *recon_c_;
     uint16_t *recon1_16bit_, *recon2_16bit_, *pred16bit_;
     int16_t *residual_;
     int32_t *residual_int32_;
