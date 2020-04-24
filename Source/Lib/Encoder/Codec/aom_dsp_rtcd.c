2d8a3295817a5b67fb8b51e17693279fed3b8e2d
Tomasz Szumski <tomasz.szumski@intel.com>
2020-04-24 07:30:36 +0200

    Fix eb_av1_wiener_convolve_add_src_avx2/avx512 for non multiple of 8 resolution

--- Source/Lib/Encoder/Codec/aom_dsp_rtcd.c
+++ Source/Lib/Encoder/Codec/aom_dsp_rtcd.c
@@ -62,10 +62,6 @@
     } while (0)
 #endif
 
-void setup_rtcd_non8(CPU_FLAGS flags) {
-    (void) flags;
-    eb_av1_wiener_convolve_add_src = eb_av1_wiener_convolve_add_src_c;
-}
 void setup_rtcd_internal(CPU_FLAGS flags) {
     /** Should be done during library initialization,
         but for safe limiting cpu flags again. */
